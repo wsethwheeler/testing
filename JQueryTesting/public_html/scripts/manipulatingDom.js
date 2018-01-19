14b093ea903ed292b0f005df8c1619dc65756964
Wyatt Seth J. Wheeler <wsethwheeler@gmail.com>
2018-01-18 21:07:49 -0500

    Basic jquery manipulation and practice

--- JQueryTesting/public_html/scripts/manipulatingDom.js
+++ JQueryTesting/public_html/scripts/manipulatingDom.js
@@ -2,23 +2,20 @@ $(document).ready(function(){
     //Create a new node... but it's not attached yet
     var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
     
-    //When ANY button is clicked (only one on our page), do this event handler...
-    $('button').on('click', function(){
-        //Select '.tour' node and attach the 'message' node to the DOM
+    $('.book').on('click', function(){
         $(this).closest('.tour').append(message);
         
-        /* Remove the button that was clicked...
-         * Important!  If 'this' were not used, ALL buttons would be removed :(
-         */
         $(this).remove(); 
     });
     
-    //Alternative: $(price).appendTo('#destinations');
-    
-    /* Other options for adding nodes to DOM:
-     *  .appendTo(<element>)
-     *  .prependTo(<element>)
-     *  .insertAfter(<element>)
-     *  .insertBefore(<element>)
-     */
+    //Add filters
+    $('#filters').on('click', '.on-sale', function () {
+        $('.highlight').removeClass('highlight');
+        $('.tour').filter('.on-sale').addClass('highlight');
+    });
+
+    $('#filters').on('click', '.featured', function () {
+        $('.highlight').removeClass('highlight');
+        $('.tour').filter('.featured').addClass('highlight');
+    });
 });
