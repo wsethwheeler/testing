2c3029dc76b7b008d7fa292c9e06cbb5da9041f6
Wyatt Seth J. Wheeler <wsethwheeler@gmail.com>
2018-01-30 21:43:30 -0500

    Added Angular test app based off codeschool

---
+++ TestAngularApp/public_html/scripts/app.js
@@ -0,0 +1,27 @@
+//Wrap app in a closure... good habit to get into
+(function () {
+    var app = angular.module('store', []);
+
+    //Creating a controller... always name them "<Something>Controller"
+    //Give it an anonymous inner function
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+    
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'It\'s super rad!',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: 'Powerful stuff...',
+            canPurchase: false,
+            soldOut: false
+        }
+    ];
+})();
